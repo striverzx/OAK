85f02ea36f361a42a29516bf11a8a4b6e793c0c4
striverzx <striverzx011@gmail.com>
2024-07-29 12:19:38 +0530

    added the new classes in java

---
+++ OOPS/Interfaces/01Interface.java
@@ -0,0 +1,40 @@
+
+interface Test1{
+	void method1();
+	void method2();
+}
+
+class Test2 implements Test1{
+
+	public void method1(){
+		System.out.println("Method1 of the Test2");
+	}
+
+	public void method2(){
+		System.out.println("Method2 of the Test2");
+	}
+
+	public void method3(){
+		System.out.println("Method3 of the Test2");
+	}
+}
+class Main{
+	public static void main(String ...args){
+
+		Test2 t2=new Test2();
+		t2.method1();
+		t2.method2();
+		t2.method3();
+
+		Test1 t1=new Test2();
+		t1.method1();
+		t1.method2();
+		// t1.method3();
+
+
+
+
+
+
+	}
+}
